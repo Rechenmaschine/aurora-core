2ebb5ce3f453f7288c4c7193eefc3befe3d6f2d2
Moritz Kuntze <mfk@zortac.net>
2023-04-18 23:39:36 +0200

    Make Clippy more happy and add pre-commit hooks

--- aurora_hal/aurora_hal_macros/src/lib.rs
+++ aurora_hal/aurora_hal_macros/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput, parse::Parser};
 use quote::quote;
+use syn::{parse::Parser, parse_macro_input, DeriveInput};
 use toml::Value;
 
 #[proc_macro_attribute]
@@ -15,19 +15,21 @@ pub fn add_fields(_args: TokenStream, input: TokenStream) -> TokenStream {
 
     //Add the fields for Control and Process Variables to the Struct
     match &mut ast.data {
-        syn::Data::Struct(ref mut struct_data) => {
-            match &mut struct_data.fields {
-                syn::Fields::Named(fields) => {
-                    fields.named
-                        .push(syn::Field::parse_named.parse2(quote! { process: ProcessVar }).unwrap());
-                    fields.named
-                        .push(syn::Field::parse_named.parse2(quote! { control: ControlVar }).unwrap());
-                }
-                _ => {
-                    ()
-                }
+        syn::Data::Struct(ref mut struct_data) => match &mut struct_data.fields {
+            syn::Fields::Named(fields) => {
+                fields.named.push(
+                    syn::Field::parse_named
+                        .parse2(quote! { process: ProcessVar })
+                        .unwrap(),
+                );
+                fields.named.push(
+                    syn::Field::parse_named
+                        .parse2(quote! { control: ControlVar })
+                        .unwrap(),
+                );
             }
-        }
+            _ => (),
+        },
         _ => {
             panic!("Add field has to be called with structs");
         }
@@ -39,18 +41,23 @@ pub fn add_fields(_args: TokenStream, input: TokenStream) -> TokenStream {
     let process_def: proc_macro2::TokenStream = match process_def_opt {
         Some(x) => x,
         None => String::new(),
-    }.parse().unwrap();
+    }
+    .parse()
+    .unwrap();
 
     let control_def: proc_macro2::TokenStream = match control_def_opt {
         Some(x) => x,
         None => String::new(),
-    }.parse().unwrap();
+    }
+    .parse()
+    .unwrap();
 
-    return quote!{
+    quote! {
         #process_def
         #control_def
         #ast
-    }.into();
+    }
+    .into()
 }
 
 fn build_struct(key: &str, value: &Value) -> (Option<String>, Option<String>) {
@@ -59,11 +66,11 @@ fn build_struct(key: &str, value: &Value) -> (Option<String>, Option<String>) {
 
     match value {
         Value::String(var_type) => {
-            struct_def.push_str(&format!("{}: {},\n", key, var_type));
-            return (Some(struct_def), None);
+            struct_def.push_str(&format!("{key}: {var_type},\n"));
+            (Some(struct_def), None)
         }
         Value::Table(table) => {
-            rest.push_str(&format!("struct {} {{\n", key));
+            rest.push_str(&format!("struct {key} {{\n"));
 
             for (k, v) in table.iter() {
                 let (struct_res, sub_structs) = build_struct(k, v);
@@ -75,17 +82,15 @@ fn build_struct(key: &str, value: &Value) -> (Option<String>, Option<String>) {
                 }
                 match sub_structs {
                     Some(defs) => {
-                        rest = format!("{}{}", defs, rest);
+                        rest = format!("{defs}{rest}");
                     }
                     None => {}
                 }
             }
             rest.push_str("}\n\n");
-            struct_def = format!("m_{}: {},\n", key, key);
-            return (Some(struct_def), Some(rest));
+            struct_def = format!("m_{key}: {key},\n");
+            (Some(struct_def), Some(rest))
         }
-        _ => {
-            return (None, None);
-        }
+        _ => (None, None),
     }
 }
