0ab83b7a43ad6be2f3c8220fcf4067b3e12c17e7
Moritz Kuntze <mfk@zortac.net>
2023-05-22 22:15:21 +0200

    Added event generator tests

--- event_gen/src/generators/one_shot_generator.rs
+++ event_gen/src/generators/one_shot_generator.rs
@@ -15,3 +15,30 @@ impl<T: 'static + std::marker::Send> EventGenerator<T, ()> for OneShotGenerator<
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::sync::mpsc;
+
+    #[test]
+    fn generates_one_event() {
+        let (s, r) = mpsc::channel();
+        let one_shot = OneShotGenerator { value: 42 };
+        one_shot.start(s);
+        assert_eq!(r.recv().unwrap(), 42);
+    }
+
+    #[test]
+    fn generates_only_one_event() {
+        let (s, r) = mpsc::channel();
+        let one_shot = OneShotGenerator { value: 42 };
+        one_shot.start(s);
+        assert_eq!(r.recv().unwrap(), 42);
+
+        assert!(
+            r.recv().is_err(),
+            "The one shot event generator produced more than one message on the channel"
+        );
+    }
+}
